4873fa7afbb249a7725fc3b92b95f28c0a4ef34b
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-24 18:44:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,9 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-    song = self.new
-    song.title = file_name.split(" - ")[1]
+    artist_name, song_name = file_name.split(" - ")
+     song = self.new(song_name)
+    song.artist_name = artist_name
     song
   end
 end
