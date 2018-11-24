97ec764d3a6495922597d4ee8579deb96a060447
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-24 18:32:04 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,11 @@
 class MP3Importer
 
+  attr_accessor :file_path
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
 
   def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
