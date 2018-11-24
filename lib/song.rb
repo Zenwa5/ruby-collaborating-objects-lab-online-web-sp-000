1eecc86bf551670abbae126cd748336abff7311e
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-24 18:45:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,4 +11,9 @@ class Song
     song.artist_name = artist_name
     song
   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
 end
